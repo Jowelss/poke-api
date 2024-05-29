8b438714b4aff4045c8af8a8713045cad9a97240
Jowelss <devejowels.gitl.loper@gmail.com>
2024-05-29 19:39:02 -0400

    change element pokemon

--- js/app.js
+++ js/app.js
@@ -1,28 +1,73 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
-  .then((res) => res.json())
-  .then((data) => {
-    data.results.forEach((element) => {
-      fetch(element.url)
-        .then((res) => res.json())
-        .then((data) => getPokemon(data))
-        .catch((err) => console.log(err));
-    });
-  })
-  .catch((err) => console.log(err));
+const imgPoke = document.createElement('img');
+imgPoke.classList.add('card-image');
+const imgPokeContent = document.getElementById('image-pokemon');
 
-const imagePoke = document.querySelector('.card-image');
 const namePoke = document.getElementById('name-pokemon');
+const weightPoke = document.getElementById('card-info__weight');
+const expPoke = document.getElementById('card-info__exp');
+const heightPoke = document.getElementById('card-info__height');
 
 const fragment = document.createDocumentFragment();
 
+const buttonBack = document.getElementById('button-back');
+const buttonNext = document.getElementById('button-next');
+
 const pokemons = [];
 
-const getPokemon = (info) => {
-  pokemons.push({
-    nombre: info.name,
-    altura: info.height,
-    peso: info.weight,
-    image: info.sprites.front_default,
-    exp: info.base_experience,
-  });
+let num = 0;
+
+const addPokeCard = () => {
+  imgPoke.src = pokemons[num].image;
+  namePoke.textContent = pokemons[num].nombre;
+  expPoke.textContent = pokemons[num].exp;
+  heightPoke.textContent = pokemons[num].altura;
+  weightPoke.textContent = pokemons[num].peso;
+  imgPokeContent.appendChild(imgPoke);
 };
+
+buttonNext.addEventListener('click', () => {
+  if (num < pokemons.length) {
+    num++;
+  }
+  addPokeCard();
+});
+
+buttonBack.addEventListener('click', () => {
+  if (num > 0) {
+    num--;
+  }
+  addPokeCard();
+});
+
+const fetchPokemonData = async () => {
+  try {
+    const response = await fetch(
+      'https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0'
+    );
+
+    const data = await response.json();
+    const pokemonPromises = data.results.map(async (element) => {
+      try {
+        const res = await fetch(element.url);
+        const info = await res.json();
+        return {
+          nombre: info.name,
+          altura: info.height,
+          peso: info.weight,
+          exp: info.base_experience,
+          image: info.sprites.front_default,
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    const pokemonData = await Promise.all(pokemonPromises);
+    pokemons.push(...pokemonData);
+    addPokeCard(pokemons);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchPokemonData();
