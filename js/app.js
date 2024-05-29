08118d2e75cc098cfe377a73431def80f252e796
Jowelss <devejowels.gitl.loper@gmail.com>
2024-05-28 20:51:44 -0400

    object pokemon

--- js/app.js
+++ js/app.js
@@ -4,8 +4,15 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
     data.results.forEach((element) => {
       fetch(element.url)
         .then((res) => res.json())
-        .then((data) => coso(data))
+        .then((data) => getPokemon(data))
         .catch((err) => console.log(err));
     });
   })
   .catch((err) => console.log(err));
+
+const getPokemon = (data) => {
+  const pokemon = {
+    nombre: data.name,
+    image: data.sprites.front_default,
+  };
+};
